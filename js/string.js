1760247e962133dba6d9b82481b77ac680db935b
Natalja <tasia@inbox.lt>
2023-06-01 10:43:31 +0300

    string lentele

---
+++ js/string.js
@@ -0,0 +1,29 @@
+const skaicius = 7 ;
+const x = 1;
+const a = 2;
+const b = 3;
+const c = 4;
+const d = 5;
+const i = 6;
+const f = 7;
+const g = 8;
+const k = 9;
+
+const lentele = `${skaicius} * 1 = ${skaicius}
+${skaicius} * ${a} = ${skaicius * a}
+${skaicius} * ${b} = ${skaicius * b}
+${skaicius} * ${c} = ${skaicius * c}
+${skaicius} * ${d} = ${skaicius * d}
+${skaicius} * ${i} = ${skaicius * i}
+${skaicius} * ${f} = ${skaicius * f}
+${skaicius} * ${g} = ${skaicius * g}
+${skaicius} * ${k} = ${skaicius * k}`;
+
+console.log(lentele);
+console.log();
+
+const daugyba = skaicius + " * " + x + " = " + skaicius + '\n' + skaicius + " * " + a + " = " + skaicius * a + '\n'+ skaicius + " * " + b + " = " + skaicius * b + "\n"+ skaicius + " * " + c + " = " + skaicius * c + "\n"+ skaicius + " * " + d + " = " + skaicius * d + "\n"+ skaicius + " * " + i + " = " + skaicius * i + "\n"+ skaicius + " * " + f + " = " + skaicius * f + "\n"+ skaicius + " * " + g + " = " + skaicius * g + "\n"+ skaicius + " * " + k + " = " + skaicius * k ; 
+
+
+console.log(daugyba);
+
